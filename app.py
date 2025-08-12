b33c8829b429ec990cf4b90dcef0430158dab09d
Chaeyeonh <strawberry@khu.ac.kr>
2025-08-12 14:24:39 +0900

    feat: 파비콘 추가

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title = "MINDoc", page_icon="💸", layout="wide"
+    page_title = "MINDoc", page_icon="images/favicon.png", layout="wide"
 )
 
 
